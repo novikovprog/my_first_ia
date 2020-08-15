77ef1166cda7056ad81dad6923da016abc5b9d6e
Novikov <novikovprog@gmail.com>
2020-08-15 21:34:03 +0200

    ajout des commentaires dans le css et js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
+var randomNumber = Math.floor(Math.random() * 100) + 1; //---------------RANDOM CHOISE JUSQU'A 100---------------------
 
 var guesses = document.querySelector('.guesses');
 var lastResult = document.querySelector('.lastResult');
@@ -6,12 +6,12 @@ var lowOrHi = document.querySelector('.lowOrHi');
 var print = document.querySelector('.print');
 print.textContent = randomNumber;
 
-var guessSubmit = document.querySelector('.guessSubmit');
-var guessField = document.querySelector('.guessField');
+var guessSubmit = document.querySelector('.guessSubmit'); //---------------BOUTON VALIDER--------------------
+var guessField = document.querySelector('.guessField'); //-----------------INPUT-----------------------------
 
 var guessCount = 1;
-var resetButton = document.createElement("button");
-
+var resetButton = document.createElement("button"); //---------------------BOUTON RECOMMENCER-----------------
+//------------------------------------------------ALGORITHME DE LA DEVINETTE----------------------------------
 function checkGuess() {
   var userGuess = Number(guessField.value);
   if (guessCount === 1) {
@@ -40,7 +40,7 @@ function checkGuess() {
   guessField.focus();
 }
 guessSubmit.addEventListener('click', checkGuess);
-
+//-------------------------------------------EVENEMENT DU BOUTON RECOMMENCER-----------------
 function setGameOver() {
   guessField.disabled = true;
   guessSubmit.disabled = true;
@@ -49,7 +49,7 @@ function setGameOver() {
   document.body.appendChild(resetButton);
   resetButton.addEventListener('click', resetGame);
 }
-
+//-------------------------------------------FONCTION DE CALCUL----------------------------
 function resetGame() {
   guessCount = 1;
 
@@ -58,15 +58,15 @@ function resetGame() {
     resetParas[i].textContent = '';
   }
 
-  resetButton.parentNode.removeChild(resetButton);
+  resetButton.parentNode.removeChild(resetButton); //-----------------------DISPARITION DU BOUTON RECOMMENCER------------
 
   guessField.disabled = false;
   guessSubmit.disabled = false;
   guessField.value = '';
   guessField.focus();
 
-  lastResult.style.backgroundColor = 'transparent';
+  lastResult.style.backgroundColor = 'transparent'; //------------------------------RESULTAT-------------------------
 
   randomNumber = Math.floor(Math.random() * 100) + 1;
-  print.textContent = randomNumber;
+  print.textContent = randomNumber; //-----------------------------AFFICHAGE DE LA REPONSE (pour être gentil)--------------------
 }
